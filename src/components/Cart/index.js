94133f51048cf16a7e1861c58e74310a3f96d845
pikkilisaikumar <pikkilisaikumar.com>
2021-11-16 01:55:21 +0000

    doubt

---
+++ src/components/Cart/index.js
@@ -0,0 +1,14 @@
+// Write your JS code here
+import './index.css'
+
+const Cart = () => (
+  <div className="backgroundone">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+      alt="cart"
+      className="productdetails"
+    />
+  </div>
+)
+
+export default Cart
---
+++ src/components/Products/index.js
@@ -0,0 +1,13 @@
+import './index.css'
+
+const Products = () => (
+  <div className="backgroundone">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+      alt="products"
+      className="productdetails"
+    />
+  </div>
+)
+
+export default Products
